b061ecec094126c0df6c984815f6d240fd16b573
Muhammad Tawheed <78700500+Tawheedop@users.noreply.github.com>
2022-09-14 12:42:13 +0530

    Update binExpIter.cpp

--- binExpIter.cpp
+++ binExpIter.cpp
@@ -1,1 +1,21 @@
-#include<bits/stdc++.h>
+#include<bits/stdc++.h> 
+using namespace std;
+const int Mod = 13;
+//log(b)
+int binExpIter(int a,int b){
+    int ans=1;
+    while(b){   //(b--)
+        if(b&1){  // (a&1==1)
+        ans = (ans* 1ll *a)%Mod;
+    }
+    a=(a* 1ll* a)%Mod;
+    b = b >> 1;
+    }
+    return ans;
+
+}
+int main()
+{
+    int a=2, b=90;
+    cout << binExpIter(a,b);
+}
